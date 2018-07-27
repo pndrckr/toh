842106e972a3f6be182ead99b60c02a9f159049b
Tati <tatiestrada@me.com>
2018-07-27 04:24:42 +0100

    Hero created

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '..hero';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+	// hero = 'Wham!';
+
+	hero: Hero = {
+		id: 1,
+		name: 'Wham!'
+	}
+
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+  }
+
+}
